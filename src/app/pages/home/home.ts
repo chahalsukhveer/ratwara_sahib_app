42cae1ce1853796fc9358212ad07128982484aea
Edwin Biemond <biemond@gmail.com>
2017-04-21 17:19:44 +0200

    fix performance

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,6 +5,16 @@ import { TranslateService } from 'ng2-translate';
 import { defaultLanguage } from '../../i18n.constants';
 import { Storage } from '@ionic/storage';
 
+export class ImageKey {
+  img: string;
+  text: string;
+  constructor(_img: string, _text: string) {
+     // assign local variables to class members
+     this.img = _img;
+     this.text = _text;
+  }
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -26,15 +36,21 @@ export class HomePage {
                     console.log('retrieved from cache');
                     console.log(val);
                     this.translate.use(val);
+                    this.addImages();
+                } else {
+                   this.addImages();
                 } 
             });
-        } 
+        } else {
+           this.addImages();
+        }
     });
   }
 
   changeLanguage(key) {
 		this.translate.use(key);
     this.storage.set('locale', key);
+    // this.addImages();
 	}
 
   ionViewDidEnter() {
@@ -42,6 +58,20 @@ export class HomePage {
           // Okay, so the platform is ready and our plugins are available.
           GoogleAnalytics.trackView("Home Page");
     });
+  }
+
+  addImages() {
+    //  this.images = [];
+    //  var entry1 = new ImageKey( "assets/images/samagam04.jpg", this.translate.instant("HOME_TEXT_1") );
+    //  var entry2 = new ImageKey( "assets/images/samagam.jpg", this.translate.instant("HOME_TEXT_2") );
+    //  var entry3 = new ImageKey( "assets/images/samagam02.jpg", this.translate.instant("HOME_TEXT_3") );
+    //  var entry4 = new ImageKey( "assets/images/samagam03.jpg", this.translate.instant("HOME_TEXT_4") );
+    //  var entry5 = new ImageKey( "assets/images/samagam01.jpg", this.translate.instant("HOME_TEXT_5") );
+    //  this.images.push(entry1);
+    //  this.images.push(entry2);
+    //  this.images.push(entry3);
+    //  this.images.push(entry4);
+    //  this.images.push(entry5);
   }
 
   images = [
