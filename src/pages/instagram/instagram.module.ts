c69bf9491b9581ea06db147e1efe7dfe72768cd0
Edwin Biemond <biemond@gmail.com>
2017-06-18 14:14:46 +0200

    optimize instagram

--- src/pages/instagram/instagram.module.ts
+++ src/pages/instagram/instagram.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InstagramPage } from './instagram';
 import { TranslateModule } from '@ngx-translate/core';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,9 @@ import { TranslateModule } from '@ngx-translate/core';
   ],
   imports: [
     IonicPageModule.forChild(InstagramPage),
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+    HttpModule,
+    JsonpModule
   ],
   exports: [
     InstagramPage
--- src/pages/instagram/instagram.ts
+++ src/pages/instagram/instagram.ts
@@ -1,19 +1,26 @@
 import { IonicPage, Platform } from 'ionic-angular';
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
-import { GoogleAnalytics } from 'ionic-native';
+import { Http, Jsonp } from '@angular/http';
+import { InAppBrowser, GoogleAnalytics } from 'ionic-native';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
 
 @IonicPage()
 @Component({
     selector: 'page-instagram',
     templateUrl: 'instagram.html'
 })
-
 export class InstagramPage {
+    url: string = 'https://igapi.ga/ratwarasahib/media/?callback=JSONP_CALLBACK';
+    photos: any;
 
-    constructor(public platform: Platform ) {
+    constructor(public platform: Platform, 
+                private http: Http,
+                private jsonp: Jsonp) {
         platform.ready().then(() => {
         });
+        this.refreshData();
     }
 
     ionViewDidEnter() {
@@ -23,4 +30,33 @@ export class InstagramPage {
          console.log("Instagram Page enter");
        });
     }
+
+    refreshData(){
+        this.getServer()
+            .subscribe(
+                res => { 
+                    this.photos = res.items;
+                    console.log(res); 
+                },
+                err => {
+                    console.log(err);  // debug
+                }
+            );    
+    }
+
+    getServer() {
+       return this.jsonp.get(this.url)
+        .map(res => res.json() )
+        .catch(error => Observable.throw(error)
+       );
+    }
+
+  openpic(url): void {
+    console.log(url);
+    this.platform.ready().then(() => {
+        let browser = new InAppBrowser(url, "_system", "location=true");
+        browser.show();
+    });
+  }
+
 }
