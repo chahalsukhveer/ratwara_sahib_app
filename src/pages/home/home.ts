1fa87e0e263b2485bf13069b94d53643cc07dcf3
chahalsukhveer <chahal.sukhveer@gmail.com>
2017-10-01 15:42:35 +0200

    update home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,23 +34,23 @@ export class HomePage {
       text: "HOME_TEXT_1",
       text_en: "VGRMC Trust was founded by Sant Waryam Singh Ji inspired by his mentor Sant Isher Singh Ji of Rara Sahib."
     },
-    { img: "assets/images/pic21.jpeg", 
+    { img: "assets/images/pic25.jpeg", 
       text: "HOME_TEXT_2",
       text_en: "The Samparada is linked with Bhai Sahib Bhai Daya Singh Ji, Leading Piara of the Five, selected by Guru Gobind Singh Ji."
     },
-    { img: "assets/images/pic22.jpeg", 
+    { img: "assets/images/pic24.jpeg", 
       text: "HOME_TEXT_3",
       text_en: "Sant Maharaj Ji founded Gurmat Parchar Centre, Gurudwara Isher Parkash at Ratwara Sahib in 1986 for spreading Gurmat (Sikh Teachings) in India and abroad"
     },
-    { img: "assets/images/pic23.jpeg", 
+    { img: "assets/images/pic21.jpeg", 
       text: "HOME_TEXT_1",
       text_en: "VGRMC Trust was founded by Sant Waryam Singh Ji inspired by his mentor Sant Isher Singh Ji of Rara Sahib."
     },
-    { img: "assets/images/pic24.jpeg", 
+    { img: "assets/images/pic22.jpeg", 
       text: "HOME_TEXT_2",
       text_en: "The Samparada is linked with Bhai Sahib Bhai Daya Singh Ji, Leading Piara of the Five, selected by Guru Gobind Singh Ji."
     },
-    { img: "assets/images/pic25.jpeg", 
+    { img: "assets/images/pic6.jpeg", 
       text: "HOME_TEXT_3",
       text_en: "Sant Maharaj Ji founded Gurmat Parchar Centre, Gurudwara Isher Parkash at Ratwara Sahib in 1986 for spreading Gurmat (Sikh Teachings) in India and abroad"
     },
@@ -58,11 +58,11 @@ export class HomePage {
       text: "HOME_TEXT_4",
       text_en: "Started publishing monthly magazine ATAM MARG in 1995"
     },
-    { img: "assets/images/pic10.jpeg", 
+    { img: "assets/images/pic23.jpeg", 
       text: "HOME_TEXT_5",
       text_en: "published books and produced videos"
     },
-    { img: "assets/images/pic11.jpeg", 
+    { img: "assets/images/pic5.jpeg", 
       text: "HOME_TEXT_3",
       text_en: "Sant Maharaj Ji founded Gurmat Parchar Centre, Gurudwara Isher Parkash at Ratwara Sahib in 1986 for spreading Gurmat (Sikh Teachings) in India and abroad"
     }
