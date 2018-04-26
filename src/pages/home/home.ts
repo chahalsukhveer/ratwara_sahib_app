16e0e18fdedcabc5ac2b31d1c11f28b366d8b193
chahalsukhveer <chahal.sukhveer@gmail.com>
2018-04-26 17:28:21 +0200

    updated pics n magazines
    updated pics n magazines

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,34 +31,34 @@ export class HomePage {
   }
 
   images = [
-     { img: "assets/images/pic1.jpeg", 
+     { img: "assets/images/pic1.jpg", 
       text: "HOME_TEXT_1",
       text_en: "VGRMC Trust was founded by Sant Waryam Singh Ji inspired by his mentor Sant Isher Singh Ji of Rara Sahib."
     },
-    { img: "assets/images/pic2.jpeg", 
+    { img: "assets/images/pic2.jpg", 
       text: "HOME_TEXT_3",
       text_en: "Sant Maharaj Ji founded Gurmat Parchar Centre, Gurudwara Isher Parkash at Ratwara Sahib in 1986 for spreading Gurmat (Sikh Teachings) in India and abroad"
     },
-    { img: "assets/images/pic3.jpeg", 
+    { img: "assets/images/pic3.jpg", 
       text: "HOME_TEXT_1",
       text_en: "VGRMC Trust was founded by Sant Waryam Singh Ji inspired by his mentor Sant Isher Singh Ji of Rara Sahib."
     },
-    { img: "assets/images/pic11.jpeg", 
+    { img: "assets/images/pic4.jpg", 
       text: "HOME_TEXT_2",
       text_en: "The Samparada is linked with Bhai Sahib Bhai Daya Singh Ji, Leading Piara of the Five, selected by Guru Gobind Singh Ji."
     },
-    { img: "assets/images/pic4.jpeg", 
-      text: "HOME_TEXT_2",
-      text_en: "The Samparada is linked with Bhai Sahib Bhai Daya Singh Ji, Leading Piara of the Five, selected by Guru Gobind Singh Ji."
-    },
-    { img: "assets/images/pic6.jpeg", 
+    { img: "assets/images/pic6.jpg", 
       text: "HOME_TEXT_3",
       text_en: "Sant Maharaj Ji founded Gurmat Parchar Centre, Gurudwara Isher Parkash at Ratwara Sahib in 1986 for spreading Gurmat (Sikh Teachings) in India and abroad"
     },
-    { img: "assets/images/pic5.jpeg", 
+    { img: "assets/images/pic5.jpg", 
       text: "HOME_TEXT_4",
       text_en: "Started publishing monthly magazine ATAM MARG in 1995"
     },
+    { img: "assets/images/pic11.jpeg", 
+    text: "HOME_TEXT_2",
+    text_en: "The Samparada is linked with Bhai Sahib Bhai Daya Singh Ji, Leading Piara of the Five, selected by Guru Gobind Singh Ji."
+   },
     { img: "assets/images/pic8.jpeg", 
       text: "HOME_TEXT_5",
       text_en: "published books and produced videos"
