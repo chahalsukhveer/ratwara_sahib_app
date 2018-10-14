915ec92290357928061806f79af582e66db1c0cd
chahalsukhveer <chahal.sukhveer@gmail.com>
2018-10-14 18:52:38 +0200

    changes for policy

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, MenuController, IonicPage } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { AppVersion } from '@ionic-native/app-version';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 
 @IonicPage()
 @Component({
@@ -15,6 +17,7 @@ export class AboutPage {
   constructor(public navCtrl: NavController, 
               public menu: MenuController,
               private platform: Platform,
+              private iab: InAppBrowser,
               private app: AppVersion ) {
     platform.ready().then(() => {
       if (this.platform.is('cordova')) {
@@ -35,5 +38,11 @@ export class AboutPage {
   closeMenu() {
     this.menu.close();
     this.navCtrl.push("TabsPage");
+  }
+  openPolicyLink(): void {
+    var url = 'https://ratwarasahibblog.wordpress.com/2018/10/02/privacy-policy/';
+    this.platform.ready().then(() => {
+        let browser = this.iab.create(url, "_system", "location=true");
+    });
   }
 }
